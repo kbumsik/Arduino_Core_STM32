7df4edfe34c7d503399ff467ebfdd52a7c352e50
Bumsik Kim <k.bumsik@gmail.com>
2019-11-07 11:31:59 +0900

    [VirtIO] Add ringbuffer

---
+++ cores/arduino/stm32/virtio/ringbuffer.c
@@ -0,0 +1,126 @@
+/**
+ * MIT License:
+ * Copyright (c) 2019 Bumsik kim <k.bumsik@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifdef VIRTIOCON
+
+#include "ringbuffer.h"
+#include <string.h>
+#include "wiring.h"
+
+#define BUFFER_END (VIRTIO_RINGBUFFER_SIZE - 1)
+
+static uint16_t read_tmp(ringbuffer_t *ring, uint8_t *dst, uint16_t size);
+static void read_commit(ringbuffer_t *ring);
+static void read_rollback(ringbuffer_t *ring);
+
+void ringbuffer_init(ringbuffer_t *ring)
+{
+  ring->write = 0;
+  ring->read = 0;
+  ring->read_tmp = 0;
+}
+
+uint16_t ringbuffer_read_available(ringbuffer_t *ring)
+{
+  // This will make the function safe when write openrations are done in interrupts
+  volatile uint16_t write = ring->write;
+
+  if (write < ring->read) {
+    return (BUFFER_END - ring->read) + (write + 1);
+  }
+  return write - ring->read;
+}
+
+static uint16_t read_tmp(ringbuffer_t *ring, uint8_t *dst, uint16_t size)
+{
+  // This will make the function safe when write openrations are done in interrupts
+  volatile uint16_t write = ring->write;
+  uint16_t end = (write >= ring->read_tmp) ? write : BUFFER_END + 1;
+
+  size = min(end - ring->read_tmp, size);
+  memcpy(dst, ring->buffer + ring->read_tmp, size);
+  ring->read_tmp += size;
+  if (ring->read_tmp > BUFFER_END) {
+    ring->read_tmp = 0;
+  }
+  return size;
+}
+
+static void read_commit(ringbuffer_t *ring)
+{
+  ring->read = ring->read_tmp;
+}
+
+static void read_rollback(ringbuffer_t *ring)
+{
+  ring->read_tmp = ring->read;
+}
+
+uint16_t ringbuffer_read(ringbuffer_t *ring, uint8_t *dst, uint16_t size)
+{
+  uint16_t recv_size = read_tmp(ring, dst, size);
+  read_commit(ring);
+  return recv_size;
+}
+
+/**
+ * WARNING: The size of read cannot be larger than ringbuffer_read_available().
+ */
+uint16_t ringbuffer_peek(ringbuffer_t *ring, uint8_t *dst, uint16_t size)
+{
+  size = min(size, ringbuffer_read_available(ring));
+  uint16_t recv_size = 0;
+  while (recv_size < size) {
+    recv_size += read_tmp(ring, dst + recv_size, size - recv_size);
+  }
+  read_rollback(ring);
+  return recv_size;
+}
+
+uint16_t ringbuffer_write_available(ringbuffer_t *ring)
+{
+  // This will make the function safe when read openrations are done in interrupts
+  volatile uint16_t read = ring->read;
+
+  if (ring->write < read) {
+    return (read - 1) - ring->write;
+  }
+  return read + (BUFFER_END - ring->write);
+}
+
+uint16_t ringbuffer_write(ringbuffer_t *ring, uint8_t *src, uint16_t size)
+{
+  // This will make the function safe when read openrations are done in a interrupt
+  volatile uint16_t read = ring->read;
+  uint16_t end = (ring->write < read) ? read - 1
+                 : (read == 0) ? BUFFER_END : BUFFER_END + 1;
+
+  size = min(end - ring->write, size);
+  memcpy(ring->buffer + ring->write, src, size);
+  ring->write += size;
+  if (ring->write > BUFFER_END) {
+    ring->write = 0;
+  }
+  return size;
+}
+
+#endif /* VIRTIOCON */
